392604318aaa40f819daace3ac6bac16003682e4
GeraldAC <200332@unsaac.edu.pe>
2024-08-26 22:18:18 -0500

    Se agrega variables de entorno

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const http = require("http")
 
 function requestController () {
@@ -6,4 +8,9 @@ function requestController () {
 
 const server = http.createServer(requestController)
 
-server.listen(4000)
+const PORT = process.env.PORT
+
+server.listen(PORT, function () {
+    console.log("Aplicación corriendo en puerto: " + PORT)
+})
+
